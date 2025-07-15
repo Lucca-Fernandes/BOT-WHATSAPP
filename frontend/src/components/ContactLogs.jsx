62aef34f62b611f6841f842a98ebbe52c8c7165c
Lucca-Fernandes <lfdc212@gmail.com>
2025-07-15 19:58:34 -0300

    link deploy

--- frontend/src/components/ContactLogs.jsx
+++ frontend/src/components/ContactLogs.jsx
@@ -19,7 +19,7 @@ function ContactLogs() {
     const [groupedLogs, setGroupedLogs] = useState({});
     const [selectedAgent, setSelectedAgent] = useState(null);
 
-    const API_URL = 'http://localhost:5000';
+    const API_URL = 'https://bot-whatsapp-1-yu8c.onrender.com';
 
     // Função para buscar os logs da API
     const fetchLogs = async () => {
